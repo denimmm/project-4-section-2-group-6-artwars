c3e2fd4742c9d3fb9741b1e55463e092e3837068
denimmm <dmacdougall7987@conestogac.on.ca>
2025-03-01 21:56:26 -0500

    created basic layout for model-view-viewmodel
    created model classes for server, state, and player

---
+++ Server/ArtWarsServer/ArtWarsServer/Model/Player.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+using System.Linq.Expressions;
+
+namespace ArtWarsServer.Model
+{
+    class Player
+    {
+        //unique identifier for each player
+        public int ID { get; set; }
+
+        public string Name { get; }
+
+        private Socket _socket;
+
+
+
+        Player(string name, Socket socket)
+        {
+            //set player name
+            this.Name = name;
+
+            //set player socket
+            _socket = socket;
+        }
+
+
+        public async Task sendData(string message, int Size)
+        {
+            try
+            {
+                if (_socket.Connected)
+                {
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    await _socket.SendAsync(data, SocketFlags.None);
+                }
+                else
+                {
+                    Console.WriteLine($" {Name}, Player ID: {ID} is not connected");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending message to player \"{Name}\" ID: {ID}. {ex.Message}");
+            }
+
+        }
+    }
+}
---
+++ Server/ArtWarsServer/ArtWarsServer/Model/Server.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtWarsServer.Model
+{
+    class Server
+    {
+        //list of players in the game
+        List<Player> Players;
+
+        private int PlayerID_Index;//this is used to keep track of the last issued player id
+
+        
+            Server()
+        {
+            //initialize players list
+            Players = new List<Player>();
+            PlayerID_Index = 0;
+        }
+
+        /// <summary>
+        /// assigns an id to a player object and adds it to the list
+        /// </summary>
+        /// <param name="p"></param>
+        void AddPlayer(Player p)
+        {
+            //increase player count
+            PlayerID_Index++;
+            //set player id
+            p.ID = PlayerID_Index;
+
+
+            Players.Add(p);
+
+        }
+
+        void RemovePlayer(Player p)
+        {
+            Players.Remove(p);
+        }
+
+    }
+}
