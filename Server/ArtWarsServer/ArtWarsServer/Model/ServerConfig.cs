d787d648ae580883f936f322b94ba50fbf3da03d
denimmm <dmacdougall7987@conestogac.on.ca>
2025-03-04 15:57:58 -0500

    added serverconfig class

---
+++ Server/ArtWarsServer/ArtWarsServer/Model/ServerConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtWarsServer.Model
+{
+    internal class ServerConfig
+    {
+        //server settings//
+        public int Port;
+        public int Min_Players;
+        public int Max_Players;
+        public int Voting_Time;
+        public int Drawing_Time;
+        public int Prompt_Writing_Time;
+        public int Results_Time;
+
+        ServerConfig()
+        {
+            Port = 25565;
+            Max_Players = 4;
+            Min_Players = 3;
+            Prompt_Writing_Time = 60;
+            Voting_Time = 60;
+            Drawing_Time = 360;
+            Results_Time = 30;
+
+        }
+
+    }
+}
