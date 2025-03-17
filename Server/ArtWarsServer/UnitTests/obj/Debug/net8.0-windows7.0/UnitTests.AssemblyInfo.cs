495807054e902a97b1b83022a9cf9e0f9eb86567
denimmm <dmacdougall7987@conestogac.on.ca>
2025-03-17 16:41:27 -0400

    small changes to connectingPage structure

--- Server/ArtWarsServer/UnitTests/obj/Debug/net8.0-windows7.0/UnitTests.AssemblyInfo.cs
+++ Server/ArtWarsServer/UnitTests/obj/Debug/net8.0-windows7.0/UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7253186c4e82d6d60b26d1b11c937bb7639b7865")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ce85acfe9b33b975136e206c689692ac089f281")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
