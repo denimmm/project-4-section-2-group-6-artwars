fc45353886ac040ccbd70b4a096a3cfe38a1e86a
BrooksSnLiu <BROOKS.SN.LIU@GMAIL.COM>
2025-03-19 17:17:33 -0400

    Finished drawing funcitonality

---
+++ ArtWarsClientWPF/DrawingCanvas.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows.Shapes;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Input;
+using System.Windows;
+
+namespace ArtWarsClientWPF
+{
+    public class DrawingCanvas : Canvas
+    {
+        private Polyline polyline;
+        private Brush currentBrush = Brushes.Black;
+        private double brushThickness = 3;
+
+        public DrawingCanvas()
+        {
+            Background = Brushes.White;
+            MouseDown += Canvas_MouseDown;
+            MouseMove += Canvas_MouseMove;
+            MouseUp += Canvas_MouseUp;
+        }
+
+        public void SetBrushColour(Brush brush)
+        {
+            currentBrush = brush;
+        }
+
+        public void TurnOnEraser()
+        {
+            currentBrush = Brushes.White;
+        }
+
+        public void SetBrushThickness(double thickness)
+        {
+            brushThickness = thickness;
+        }
+
+        public void ClearCanvas()
+        {
+            Children.Clear();
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            polyline = new Polyline
+            {
+                Stroke = currentBrush,
+                StrokeThickness = brushThickness
+            };
+            polyline.Points.Add(e.GetPosition(this));
+            Children.Add(polyline);
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed && polyline != null)
+            {
+                polyline.Points.Add(e.GetPosition(this));
+            }
+        }
+
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            polyline = null;
+        }
+    }
+}
+
---
+++ ArtWarsClientWPF/DrawingWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace ArtWarsClientWPF
+{
+    public partial class DrawingWindow : Window
+    {
+        public DrawingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is System.Windows.Controls.Button button && button.Tag is string colorName)
+            {
+                CanvasArea.SetBrushColour((Brush)new BrushConverter().ConvertFromString(colorName));
+            }
+        }
+
+        private void Erase_Click(object sender, RoutedEventArgs e)
+        {
+            CanvasArea.TurnOnEraser();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            CanvasArea.ClearCanvas();
+        }
+
+        private void BrushSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            CanvasArea.SetBrushThickness(e.NewValue);
+        }
+    }
+}
+
